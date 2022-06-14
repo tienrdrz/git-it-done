b3f7465a4d1d5dca2976dd6cfb54fd44fc3b109d
etienne <etiennerodriguez3@gmail.com>
2022-06-14 11:26:31 -0400

    can view issues, message displays if >30

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function(repo) {
     console.log(repo);
@@ -9,6 +10,11 @@ var getRepoIssues = function(repo) {
       response.json().then(function(data) {
           //pass response data to dom funciton
         displayIssues(data);
+
+        //check if api paginated issues
+        if (response.headers.get("Link")) {
+            displayWarning(repo);
+        }
       });
     }
     else {
@@ -53,4 +59,16 @@ for (var i = 0; i < issues.length; i++) {
 }
 };
 
-getRepoIssues("tienrdrz/run-buddy");
+var displayWarning = function(repo) {
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    //append to warning container
+    limitWarningEl.appendChild(linkEl);
+};
+
+getRepoIssues("expressjs/express");
