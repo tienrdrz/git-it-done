a6ad1124323a904eb36a7bff619603a3905ae83b
etienne <etiennerodriguez3@gmail.com>
2022-06-14 11:54:46 -0400

    finished

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,23 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+
+var getRepoName = function() {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+  
+    if (repoName) {
+      // display repo name on the page
+      repoNameEl.textContent = repoName;
+  
+      getRepoIssues(repoName);
+    } else {
+      // if no repo was given, redirect to the homepage
+      document.location.replace("./index.html");
+    }
+  };
 
 var getRepoIssues = function(repo) {
     console.log(repo);
@@ -17,9 +35,10 @@ var getRepoIssues = function(repo) {
         }
       });
     }
-    else {
-      alert("There was a problem with your request!");
-    }
+    // else {
+    //     // if not successful, redirect to homepage
+    //     document.location.replace("./index.html");
+    //   }
   });
 };
 
@@ -71,4 +90,5 @@ var displayWarning = function(repo) {
     limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("expressjs/express");
+getRepoIssues();
+getRepoName();
